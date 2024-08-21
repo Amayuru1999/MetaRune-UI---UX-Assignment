bfbafd3506f6d4aeeb9b7cc745bf87139a154404
Amayuru1999 <amarasingheau@gmail.com>
2024-08-21 14:36:48 +0530

    chore: Update Hero component layout and add image

--- my-project/src/components/Hero/Hero.tsx
+++ my-project/src/components/Hero/Hero.tsx
@@ -1,14 +1,21 @@
+import pic1 from '../../assets/1.jpg'
 const Hero = () => {
   return (
     <div className="w-full border border-black flex">
-      <div className="w-[720px] border border-black h-[720px] flex items-start">
+      <div className="w-[720px] border border-black h-[720px] flex items-start p-10">
         <div className="flex flex-col justify-start p-4"> 
           <h1 className="text-6xl text-left">Kyiv</h1> 
           <h1 className="text-6xl text-left">LuxeBouquets</h1> 
+          <p className='text-left'>Discover Uniquely Crafted Bouquets and Gifts for Any Occasion: Spread Joy with Our Online Flower Delivery Service</p>
+          <div>
+            <div className='border border-black w-full'></div>
+          <img src={pic1} alt="flowers" className="w-[256px] h-[256px] object-cover"/>
         </div>
+        </div>
+        
       </div>
     </div>
   );
 }
-
+  
 export default Hero;
