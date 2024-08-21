871a055d59abdf07940616da14a06889e4494f93
Amayuru1999 <amarasingheau@gmail.com>
2024-08-21 14:27:15 +0530

    chore: Add Hero component to App

---
+++ my-project/src/components/Hero/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="w-full border border-black flex">
+      <div className="w-[720px] border border-black h-[720px] flex items-start">
+        <div className="flex flex-col justify-start p-4"> 
+          <h1 className="text-6xl text-left">Kyiv</h1> 
+          <h1 className="text-6xl text-left">LuxeBouquets</h1> 
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
