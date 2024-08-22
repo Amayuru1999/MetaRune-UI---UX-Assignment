e2ba437047a435834e27ff0bdda99b49a16269de
Amayuru1999 <amarasingheau@gmail.com>
2024-08-22 13:51:54 +0530

    chore: Update Contact component layout and add phone and address fields

--- my-project/src/components/Contact/Contact.tsx
+++ my-project/src/components/Contact/Contact.tsx
@@ -1,7 +1,7 @@
 const Contact = () => {
   return (
     <div className="flex">
-      <div className="w-[695px]  border border-black h-[620px] flex items-start">
+      <div className="w-[695px]  border border-black h-[620px] flex-col items-start">
         <div className="flex flex-col p-10 text-left">
           <h1 className="text-6xl">To Contact Us</h1>
           <h2 className="mt-5 text-2xl">We will call you back</h2>
@@ -13,7 +13,16 @@ const Contact = () => {
               <h1>BOOK A CALL</h1>
             </div>
           </div>
+          
         </div>
+        <div className="flex border-t border-black mt-5">
+            <div className="border-b border-r border-black w-[350px] h-[50px] p-2">
+                <h1 className="text-2xl">Phone</h1>
+            </div>
+            <div className="border-b  border-black w-[350px] h-[50px] p-2">
+                <h1 className="text-2xl">Address</h1>
+            </div>
+          </div>
       </div>
     </div>
   );
