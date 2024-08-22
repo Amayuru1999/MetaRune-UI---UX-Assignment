5ce3327b713e7b465b9e17bd046919271266766c
Amayuru1999 <amarasingheau@gmail.com>
2024-08-22 10:38:26 +0530

    refactor: Update AboutUs component layout and content

--- my-project/src/components/AboutUs/AboutUs.tsx
+++ my-project/src/components/AboutUs/AboutUs.tsx
@@ -9,8 +9,8 @@ const AboutUs = () => {
       <div className="w-[520px]  border border-black h-[520px] flex  p-10">
         <div className="flex flex-col justify-start items-start">
         <h2>OUR STORY</h2>
-        <h1 className="text-3xl">Kyiv LuxeBouquets</h1>
-        <h2 >We are a modern local floral studio, which specializes in the design and delivery of unique bouquets. We have the best florists who carefully select each look, our studio cooperates directly with farms for growing different flowers, so we always have fresh flowers, which are collected by our florists in exquisite bouquets. We have a collection of fresh bouquets, collections of dried bouquets, house plants, as well as fragrant candles from luxury brands to create the perfect atmosphere. Make someone's day amazing by sending flowers, plants and gifts the same or next day. Ordering flowers online has never been easier.</h2>
+        <h1 className="text-3xl mt-2">Kyiv LuxeBouquets</h1>
+        <h2 className="text-left mt-2">We are a modern local floral studio, which specializes in the design and delivery of unique bouquets. We have the best florists who carefully select each look, our studio cooperates directly with farms for growing different flowers, so we always have fresh flowers, which are collected by our florists in exquisite bouquets. We have a collection of fresh bouquets, collections of dried bouquets, house plants, as well as fragrant candles from luxury brands to create the perfect atmosphere. Make someone's day amazing by sending flowers, plants and gifts the same or next day. Ordering flowers online has never been easier.</h2>
         </div>
             
       </div>
